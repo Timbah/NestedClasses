bf715fb7450876fae4856c1a0ea379a1bf81eb87
Thembelani Ngema <thembelaningema20@gmail.com>
2025-10-18 12:00:34 +0200

    My version

--- src/main/java/Meal/Meal.java
+++ src/main/java/Meal/Meal.java
@@ -1,10 +1,13 @@
 package Meal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
 
     private double price = 5;
 
-    private Item burger;
+    private Burger burger;
     private Item drink;
     private Item side;
 
@@ -17,21 +20,74 @@ public class Meal {
     public Meal(double conversionRate) {
 
         this.conversionRate = conversionRate;
-        burger = new Item("regular", "burger");
+        burger = new Burger("regular", "burger");
         drink = new Item("coke", "drink", 1.5);
-        System.out.println(drink.name);
+//        System.out.println(drink.name);
         side = new Item("fries", "side", 2.0);
+    }
+
+    public void addTopping(String... toppings) {
+        burger.addBurgerTopping(toppings);
     }
 
     public double getTotal() {
 
-        double total = burger.price + drink.price + side.price;
+        double total = burger.getBurgerPrice() + drink.price + side.price;
         return Item.getPrice(total, conversionRate);
     }
 
     @Override
     public String toString() {
         return "%s%n%s%n%s%n%26sR%.2f".formatted(burger, drink, side, "Total Due:", getTotal());
+    }
+
+    private class Burger extends Item {
+
+        private List<Item> BurgertoppingsList;
+
+        public Burger(String name, String type) {
+            this(name, type, 8);
+        }
+
+        public Burger(String name, String type, double price) {
+            super(name, type, price);
+            BurgertoppingsList = new ArrayList<>();
+        }
+
+        private void addBurgerTopping(String... toppings) {
+            String[] toppingsList = toppings;
+
+            for (String topping : toppingsList) {
+                double toppingPrice = switch (topping.toLowerCase()) {
+                    case "cheese" -> 1.0;
+                    case "bacon" -> 1.5;
+                    case "lettuce" -> 0.5;
+                    default -> 0.0;
+                };
+                BurgertoppingsList.add(new Item(topping, "Topping", toppingPrice));
+            }
+        }
+
+        private double getBurgerPrice() {
+
+            double total = this.getPrice();
+            for (Item topping : BurgertoppingsList) {
+                total += topping.getPrice();
+            }
+            return total;
+        }
+
+        @Override
+        public String toString() {
+
+            System.out.printf("%10s %.2f%n", this.getClass().getSimpleName(), getBurgerPrice());
+            StringBuilder sb = new StringBuilder();
+            for (Item topping : BurgertoppingsList) {
+                sb.append(topping.toString());
+            }
+
+            return sb.toString();
+        }
     }
 
     private class Item {
@@ -57,6 +113,10 @@ public class Meal {
 
         private static double getPrice(double price, double rate) {
             return price * rate;
+        }
+
+        double getPrice() {
+            return this.price;
         }
     }
 
--- src/main/java/Meal/Store.java
+++ src/main/java/Meal/Store.java
@@ -4,9 +4,10 @@ public class Store {
 
     public static void main(String[] args) {
         Meal regularMeal = new Meal();
+        regularMeal.addTopping("lettuce");
         System.out.println(regularMeal);
 
-        Meal USRegularMeal = new Meal(17.56);
-        System.out.println(USRegularMeal);
+//        Meal USRegularMeal = new Meal(17.56);
+//        System.out.println(USRegularMeal);
     }
 }
